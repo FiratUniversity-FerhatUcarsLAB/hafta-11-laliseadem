47f03ce3c2e8eba3d6978e7792839fdbdebdd751
Lalise Adem <240541610@firat.edu.tr>
2025-12-04 05:58:41 -0800

    Implement date formatting methods and user input

--- Exercise4_1.java
+++ Exercise4_1.java
@@ -1,14 +1,37 @@
+import java.util.Scanner;
+
 public class Exercise4_1 {
 
     public static void printAmerican(String day, int date, String month, int year) {
-        // TODO
+        // Örnek: Monday, July 22, 2019
+        System.out.println(day + ", " + month + " " + date + ", " + year);
     }
 
     public static void printEuropean(String day, int date, String month, int year) {
-        // TODO
+        // Örnek: 22 July 2019, Monday
+        System.out.println(date + " " + month + " " + year + ", " + day);
     }
 
     public static void main(String[] args) {
-        // TODO: test methods
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter day (e.g. Monday): ");
+        String day = input.nextLine();
+
+        System.out.print("Enter month (e.g. July): ");
+        String month = input.nextLine();
+
+        System.out.print("Enter date (e.g. 22): ");
+        int date = input.nextInt();
+
+        System.out.print("Enter year (e.g. 2019): ");
+        int year = input.nextInt();
+
+        System.out.println("\nAmerican Format:");
+        printAmerican(day, date, month, year);
+
+        System.out.println("European Format:");
+        printEuropean(day, date, month, year);
     }
 }
