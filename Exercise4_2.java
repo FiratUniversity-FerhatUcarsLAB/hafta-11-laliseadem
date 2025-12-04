564cab10ec43f35bb082512d60b3643297042b81
Lalise Adem <240541610@firat.edu.tr>
2025-12-04 06:32:12 -0800

    Exercise4_2.java is added.
    Added line numbers as comments for clarity.

--- Exercise4_2.java
+++ Exercise4_2.java
@@ -1,23 +1,29 @@
+//Adi Soyadi: Lalise Adem Wado.
+//Ogrenci numara: 240541610.
+
+//Her satirin sonuna calisma sirasi yorumlarini ekleyin.
 public class Exercise4_2 {
 
-  // Her satirin sonuna calisma sirasi yorumlarini ekleyin.
+    public static void main(String[] args) {  // #1
+        zippo("rattle", 13);  // #2
+    } // #16
 
-    public static void main(String[] args) {
-        zippo("rattle", 13);
-    }
+    public static void baffle(String blimp) {   // #7
+        System.out.println(blimp);  // #8
+        zippo("ping", -5);          // #9
+    } // #13
 
-    public static void baffle(String blimp) {
-        System.out.println(blimp);
-        zippo("ping", -5);
-    }
-
-    public static void zippo(String quince, int flag) {
-        if (flag < 0) {
-            System.out.println(quince + " zoop");
+    public static void zippo(String quince, int flag) { // #3 , #10
+        if (flag < 0) {                // #4 , #11
+            System.out.println(quince + " zoop");  // #12
         } else {
-            System.out.println("ik");
-            baffle(quince);
-            System.out.println("boo-wa-ha-ha");
+            System.out.println("ik");       // #5
+            baffle(quince);                 // #6
+            System.out.println("boo-wa-ha-ha"); // #14
         }
-    }
+    } // #15
 }
+
+
+
+
